92e23550d04229e9766f1c427d3e42d74ca872c9
hoang1998zl <hhoaang@gmail.com>
2023-09-04 11:18:29 +0700

    .

--- restaurant_db.sql
+++ restaurant_db.sql
@@ -1,179 +1,177 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Tạo cơ sở dữ liệu
+CREATE DATABASE IF NOT EXISTS restaurant_db;
 
-CREATE TABLE `food` (
-  `food_id` int NOT NULL AUTO_INCREMENT,
-  `food_name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `type_id` int DEFAULT NULL,
-  PRIMARY KEY (`food_id`),
-  KEY `type_id` (`type_id`),
-  CONSTRAINT `food_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `food_type` (`type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Sử dụng cơ sở dữ liệu
+USE restaurant_db;
 
-CREATE TABLE `food_type` (
-  `type_id` int NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng user
+CREATE TABLE IF NOT EXISTS user (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    full_name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
 
-CREATE TABLE `like_res` (
-  `user_id` int DEFAULT NULL,
-  `res_id` int DEFAULT NULL,
-  `date_like` datetime DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `res_id` (`res_id`),
-  CONSTRAINT `like_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `like_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `restaurant` (`res_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng rate_res
+CREATE TABLE IF NOT EXISTS rate_res (
+    user_id INT,
+    res_id INT,
+    amount INT,
+    date_rate DATETIME,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (res_id) REFERENCES restaurant(res_id)
+);
 
-CREATE TABLE `order_table` (
-  `user_id` int DEFAULT NULL,
-  `food_id` int DEFAULT NULL,
-  `amount` int DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `arr_sub_id` varchar(255) DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `food_id` (`food_id`),
-  CONSTRAINT `order_table_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `order_table_ibfk_2` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng restaurant
+CREATE TABLE IF NOT EXISTS restaurant (
+    res_id INT PRIMARY KEY AUTO_INCREMENT,
+    res_name VARCHAR(255),
+    image VARCHAR(255),
+    description VARCHAR(255)
+);
 
-CREATE TABLE `rate_res` (
-  `user_id` int DEFAULT NULL,
-  `res_id` int DEFAULT NULL,
-  `amount` int DEFAULT NULL,
-  `date_rate` datetime DEFAULT NULL,
-  KEY `user_id` (`user_id`),
-  KEY `res_id` (`res_id`),
-  CONSTRAINT `rate_res_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `rate_res_ibfk_2` FOREIGN KEY (`res_id`) REFERENCES `restaurant` (`res_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng like_res
+CREATE TABLE IF NOT EXISTS like_res (
+    user_id INT,
+    res_id INT,
+    date_like DATETIME,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (res_id) REFERENCES restaurant(res_id)
+);
 
-CREATE TABLE `restaurant` (
-  `res_id` int NOT NULL AUTO_INCREMENT,
-  `res_name` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`res_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng food_type
+CREATE TABLE IF NOT EXISTS food_type (
+    type_id INT PRIMARY KEY AUTO_INCREMENT,
+    type_name VARCHAR(255)
+);
 
-CREATE TABLE `sub_food` (
-  `sub_id` int NOT NULL AUTO_INCREMENT,
-  `sub_name` varchar(255) DEFAULT NULL,
-  `sub_price` float DEFAULT NULL,
-  `food_id` int DEFAULT NULL,
-  PRIMARY KEY (`sub_id`),
-  KEY `food_id` (`food_id`),
-  CONSTRAINT `sub_food_ibfk_1` FOREIGN KEY (`food_id`) REFERENCES `food` (`food_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng food
+CREATE TABLE IF NOT EXISTS food (
+    food_id INT PRIMARY KEY AUTO_INCREMENT,
+    food_name VARCHAR(255),
+    image VARCHAR(255),
+    price FLOAT,
+    description VARCHAR(255),
+    type_id INT,
+    FOREIGN KEY (type_id) REFERENCES food_type(type_id)
+);
 
-CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `full_name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- Tạo bảng order_table
+CREATE TABLE IF NOT EXISTS order_table (
+    user_id INT,
+    food_id INT,
+    amount INT,
+    code VARCHAR(255),
+    arr_sub_id VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (food_id) REFERENCES food(food_id)
+);
 
-INSERT INTO `food` (`food_id`, `food_name`, `image`, `price`, `description`, `type_id`) VALUES
-(1, 'Phở chay', 'pho-chay.jpg', 5.99, 'Món phở chay Việt Nam', 1);
-INSERT INTO `food` (`food_id`, `food_name`, `image`, `price`, `description`, `type_id`) VALUES
-(2, 'Kem dâu', 'kem-dau.jpg', 3.49, 'Kem tráng miệng vị dâu', 1);
-INSERT INTO `food` (`food_id`, `food_name`, `image`, `price`, `description`, `type_id`) VALUES
-(3, 'Tôm hấp', 'tom-hap.jpg', 12.99, 'Tôm hấp ngon miệng', 2);
-INSERT INTO `food` (`food_id`, `food_name`, `image`, `price`, `description`, `type_id`) VALUES
-(4, 'Bò nướng', 'bo-nuong.jpg', 15.99, 'Món thịt bò nướng', 3),
-(5, 'Nước chanh', 'nuoc-chanh.jpg', 1.99, 'Nước chanh tươi', 3);
+-- Tạo bảng sub_food
+CREATE TABLE IF NOT EXISTS sub_food (
+    sub_id INT PRIMARY KEY AUTO_INCREMENT,
+    sub_name VARCHAR(255),
+    sub_price FLOAT,
+    food_id INT,
+    FOREIGN KEY (food_id) REFERENCES food(food_id)
+);
 
-INSERT INTO `food_type` (`type_id`, `type_name`) VALUES
-(1, 'Món chay');
-INSERT INTO `food_type` (`type_id`, `type_name`) VALUES
-(2, 'Món tráng miệng');
-INSERT INTO `food_type` (`type_id`, `type_name`) VALUES
-(3, 'Món hải sản');
-INSERT INTO `food_type` (`type_id`, `type_name`) VALUES
-(4, 'Món thịt'),
-(5, 'Món nước');
+INSERT INTO `user` (full_name, email, password)
+VALUES
+('user1', 'user1@example.com', 'password1'),
+('user2', 'user2@example.com', 'password2'),
+('user3', 'user3@example.com', 'password3'),
+('user4', 'user4@example.com', 'password4'),
+('user5', 'user5@example.com', 'password5');
 
-INSERT INTO `like_res` (`user_id`, `res_id`, `date_like`) VALUES
-(1, 1, '2022-01-01 00:00:00');
-INSERT INTO `like_res` (`user_id`, `res_id`, `date_like`) VALUES
-(2, 1, '2022-01-02 00:00:00');
-INSERT INTO `like_res` (`user_id`, `res_id`, `date_like`) VALUES
-(3, 2, '2022-01-03 00:00:00');
-INSERT INTO `like_res` (`user_id`, `res_id`, `date_like`) VALUES
-(4, 2, '2022-01-04 00:00:00'),
-(5, 3, '2022-01-05 00:00:00');
+INSERT INTO food_type (type_id, type_name) VALUES
+(0, "Món chay"),
+(0, "Món tráng miệng"),
+(0, "Món hải sản"),
+(0, "Món thịt"),
+(0, "Món nước");
 
-INSERT INTO `order_table` (`user_id`, `food_id`, `amount`, `code`, `arr_sub_id`) VALUES
-(1, 1, 2, 'ABC123', '1,2');
-INSERT INTO `order_table` (`user_id`, `food_id`, `amount`, `code`, `arr_sub_id`) VALUES
-(2, 3, 1, 'DEF456', '3');
-INSERT INTO `order_table` (`user_id`, `food_id`, `amount`, `code`, `arr_sub_id`) VALUES
-(3, 2, 3, 'GHI789', '2,4,5');
-INSERT INTO `order_table` (`user_id`, `food_id`, `amount`, `code`, `arr_sub_id`) VALUES
+
+INSERT INTO food (food_id, food_name, image, price, description, type_id) VALUES
+(1, "Phở chay", "pho-chay.jpg", 5.99, "Món phở chay Việt Nam", 1),
+(2, "Kem dâu", "kem-dau.jpg", 3.49, "Kem tráng miệng vị dâu", 1),
+(3, "Tôm hấp", "tom-hap.jpg", 12.99, "Tôm hấp ngon miệng", 2),
+(4, "Bò nướng", "bo-nuong.jpg", 15.99, "Món thịt bò nướng", 3),
+(5, "Nước chanh", "nuoc-chanh.jpg", 1.99, "Nước chanh tươi", 3);
+
+INSERT INTO like_res (user_id, res_id, date_like)
+VALUES
+(1, 1, '2022-01-01'),
+(2, 1, '2022-01-02'),
+(3, 2, '2022-01-03'),
+(4, 2, '2022-01-04'),
+(5, 3, '2022-01-05');
+
+INSERT INTO order_table (user_id, food_id, amount, code, arr_sub_id)
+VALUES
+(1, 1, 2, 'ABC123', '1,2'),
+(2, 3, 1, 'DEF456', '3'),
+(3, 2, 3, 'GHI789', '2,4,5'),
 (4, 4, 2, 'JKL012', '3,4'),
 (5, 5, 1, 'MNO345', '1');
 
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(1, 1, 4, '2022-01-01 00:00:00');
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(2, 1, 5, '2022-01-02 00:00:00');
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(3, 2, 3, '2022-01-03 00:00:00');
-INSERT INTO `rate_res` (`user_id`, `res_id`, `amount`, `date_rate`) VALUES
-(4, 2, 4, '2022-01-04 00:00:00'),
-(5, 3, 2, '2022-01-05 00:00:00');
+INSERT INTO restaurant (res_name, image, description)
+VALUES
+('Nhà hàng A', 'restaurant_a.jpg', 'Nhà hàng sang trọng với các món ăn đa dạng'),
+('Nhà hàng B', 'restaurant_b.jpg', 'Nhà hàng phục vụ các món hải sản tươi ngon'),
+('Nhà hàng C', 'restaurant_c.jpg', 'Nhà hàng chuyên về món thịt nướng'),
+('Nhà hàng D', 'restaurant_d.jpg', 'Nhà hàng phục vụ các món chay ngon miệng'),
+('Nhà hàng E', 'restaurant_e.jpg', 'Nhà hàng chuyên về món tráng miệng và đồ uống');
 
-INSERT INTO `restaurant` (`res_id`, `res_name`, `image`, `description`) VALUES
-(1, 'Nhà hàng A', 'restaurant_a.jpg', 'Nhà hàng sang trọng với các món ăn đa dạng');
-INSERT INTO `restaurant` (`res_id`, `res_name`, `image`, `description`) VALUES
-(2, 'Nhà hàng B', 'restaurant_b.jpg', 'Nhà hàng phục vụ các món hải sản tươi ngon');
-INSERT INTO `restaurant` (`res_id`, `res_name`, `image`, `description`) VALUES
-(3, 'Nhà hàng C', 'restaurant_c.jpg', 'Nhà hàng chuyên về món thịt nướng');
-INSERT INTO `restaurant` (`res_id`, `res_name`, `image`, `description`) VALUES
-(4, 'Nhà hàng D', 'restaurant_d.jpg', 'Nhà hàng phục vụ các món chay ngon miệng'),
-(5, 'Nhà hàng E', 'restaurant_e.jpg', 'Nhà hàng chuyên về món tráng miệng và đồ uống');
+INSERT INTO rate_res (user_id, res_id, amount, date_rate)
+VALUES
+(1, 1, 4, '2022-01-01'),
+(2, 1, 5, '2022-01-02'),
+(3, 2, 3, '2022-01-03'),
+(4, 2, 4, '2022-01-04'),
+(5, 3, 2, '2022-01-05');
 
-INSERT INTO `sub_food` (`sub_id`, `sub_name`, `sub_price`, `food_id`) VALUES
-(1, 'Sub 1', 2.99, 1);
-INSERT INTO `sub_food` (`sub_id`, `sub_name`, `sub_price`, `food_id`) VALUES
-(2, 'Sub 2', 3.99, 1);
-INSERT INTO `sub_food` (`sub_id`, `sub_name`, `sub_price`, `food_id`) VALUES
-(3, 'Sub 3', 4.99, 2);
-INSERT INTO `sub_food` (`sub_id`, `sub_name`, `sub_price`, `food_id`) VALUES
-(4, 'Sub 4', 2.49, 2),
-(5, 'Sub 5', 3.49, 3),
-(6, 'Sub 1', 2.99, 1),
-(7, 'Sub 2', 3.99, 1),
-(8, 'Sub 3', 4.99, 2),
-(9, 'Sub 4', 2.49, 2),
-(10, 'Sub 5', 3.49, 3);
+INSERT INTO sub_food (sub_name, sub_price, food_id)
+VALUES
+('Sub 1', 2.99, 1),
+('Sub 2', 3.99, 1),
+('Sub 3', 4.99, 2),
+('Sub 4', 2.49, 2),
+('Sub 5', 3.49, 3);
 
-INSERT INTO `user` (`user_id`, `full_name`, `email`, `password`) VALUES
-(1, 'user1', 'user1@example.com', 'password1');
-INSERT INTO `user` (`user_id`, `full_name`, `email`, `password`) VALUES
-(2, 'user2', 'user2@example.com', 'password2');
-INSERT INTO `user` (`user_id`, `full_name`, `email`, `password`) VALUES
-(3, 'user3', 'user3@example.com', 'password3');
-INSERT INTO `user` (`user_id`, `full_name`, `email`, `password`) VALUES
-(4, 'user4', 'user4@example.com', 'password4'),
-(5, 'user5', 'user5@example.com', 'password5');
+--Tìm 5 người đã like nhà hàng nhiều nhất
+SELECT user_id, COUNT(*) AS like_count
+FROM like_res
+GROUP BY user_id
+ORDER BY like_count DESC
+LIMIT 5;
 
+--Tìm 2 nhà hàng có lượt like nhiều nhất
+SELECT res_name, COUNT(*) AS like_count
+FROM like_res
+JOIN restaurant ON like_res.res_id = restaurant.res_id
+GROUP BY like_res.res_id, res_name
+ORDER BY like_count DESC
+LIMIT 2;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+--Tìm người đã đặt hàng nhiều nhất.
+SELECT user_id, COUNT(*) AS order_count
+FROM order_table
+GROUP BY user_id
+ORDER BY order_count DESC
+LIMIT 1;
+
+--Tìm người dùng không hoạt động trong hệ thống (không đặt hàng, không like, không đánh giá nhà hàng).
+SELECT user.user_id, user.full_name
+FROM user
+LEFT JOIN order_table ON user.user_id = order_table.user_id
+LEFT JOIN like_res ON user.user_id = like_res.user_id
+LEFT JOIN rate_res ON user.user_id = rate_res.user_id
+WHERE order_table.user_id IS NULL
+AND like_res.user_id IS NULL
+AND rate_res.user_id IS NULL;
+
+--Tính trung bình sub_food của một food.
+SELECT food_id, AVG(sub_price) AS average_sub_price
+FROM sub_food
+GROUP BY food_id;
